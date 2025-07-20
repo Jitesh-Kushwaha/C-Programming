f26cfee96114b66a476116f0bf094c77c2a23954
Jitesh-Kushwaha <jiteshkushwaha4115@gmail.com>
2025-07-20 18:32:30 +0530

    Add files via upload

---
+++ C Programming/34_arrays.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    int numbers[] = {10, 20, 30, 40, 50};
+    char grades[] = {'A', 'B', 'C', 'D', 'E'};
+    char name[] = "Jitesh";
+
+    printf("%d \n",numbers[0]);
+    printf("%c \n",grades[1]);
+    printf("%c \n",name[2]);
+    
+
+    return 0;
+}
